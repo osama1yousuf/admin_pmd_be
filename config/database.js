74cdf15c8e8ae33134dd4ef22c3b12ffc56373b2
Muhammad Osama <muhammad.osama@eusopht.com>
2024-07-19 13:23:11 -0700

    Update changes on mongodbconnection

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose
      .connect(process.env.DB_LOCAL_URI, {  
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // useCreateIndex: true,
     })
     .then((con) => {
       console.log(
