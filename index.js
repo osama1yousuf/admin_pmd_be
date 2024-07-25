07bc868e8ff021afc273b7391cfa22219cbdd8d0
Muhammad Osama <muhammad.osama@eusopht.com>
2024-07-25 11:43:09 -0700

    change on index js file

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 // const dotenv = require('dotenv');
 const path = require("path");
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: "GET,PUT,POST,DELETE", // Set the allowed methods
-//   })
-// );
+const corsOptions = {
+  origin: "http://localhost:3030", // Allow only this origin
+  methods: "GET,POST,PUT,DELETE", // Allow only these methods
+  allowedHeaders: "Content-Type,Authorization", // Allow only these headers
+};
 
-
-app.use(cors());
-
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
